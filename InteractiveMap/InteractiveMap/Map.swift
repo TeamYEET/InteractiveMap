8159b694641dd97e0b28550cea95586a2dbd660a
Christian Zurakowski <1024243@studio2-11468.fsstudio2.student.ccgs.wa.edu.au>
2017-10-18 13:57:30 +0800

    zoomable map

---
+++ InteractiveMap/InteractiveMap/Map.swift
@@ -0,0 +1,46 @@
+//
+//  Map.swift
+//  InteractiveMap
+//
+//  Created by Christian Zurakowski on 18/10/17.
+//  Copyright © 2017 YEET. All rights reserved.
+//
+
+import UIKit
+
+class Map: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var appMap: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 12.0
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return appMap
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
